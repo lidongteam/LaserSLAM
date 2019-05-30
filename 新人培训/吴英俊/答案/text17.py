0090e6f92b7e5c090c1a973b8733d8427597d9e7
ChenSiji <602922700@qq.com>
2019-05-30 22:31:08 +0800

    codereview吴英俊的代码

--- 新人培训/吴英俊/答案/text17.py
+++ 新人培训/吴英俊/答案/text17.py
@@ -1,5 +1,6 @@
 #输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
 import string
+import time
 s=input('Please enter a string:')
 letters=0
 space=0
@@ -15,3 +16,7 @@ for i in s :
   else:
     others+=1
 print('letters=%d,space=%d,digit=%d,others=%d' %(letters,space,digit,others))
+
+# python3 有一种"fstring"的概念，举个栗子给你看看
+print(f"letters={letters},space={space},digit={digit},others={others}")
+time.sleep(5)
